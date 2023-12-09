d20570a6c4a78c35625b54892238856f11578466
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2023-12-09 16:52:45 +0530

    Update New Text Document (2).py

--- New Text Document (2).py
+++ New Text Document (2).py
@@ -6,4 +6,4 @@ def multiply_numbers(num1, num2):
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 product_result = multiply_numbers(num1, num2)
-print(f"The product of {num1} and {num2} is: {product_result}")
+print(f"The product of {num1} and {num2} is: {product_result}"
